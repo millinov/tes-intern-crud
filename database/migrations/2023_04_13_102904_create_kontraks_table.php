0a65c8b8d08927705f4b209771e3c5bc902a403d
Millinov <milinovadmiral@gmail.com>
2023-04-13 17:44:29 +0700

    Create migrasi tabel kontrak

--- database/migrations/2023_04_13_102904_create_kontraks_table.php
+++ database/migrations/2023_04_13_102904_create_kontraks_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('kontraks', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('lama_kontrak');
+            $table->string('gaji_per_bulan');
         });
     }
 
