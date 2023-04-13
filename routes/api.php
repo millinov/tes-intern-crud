fdac0b13ad9a48cd6a6a28c30770f028f50dde43
Millinov <milinovadmiral@gmail.com>
2023-04-13 20:25:04 +0700

    nambah route kotrak dan jabatan

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\KontrakController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('pegawai', PegawaiController::class);
+Route::resource('jabatan', JabatanController::class);
+Route::resource('kontrak', KontrakController::class);
