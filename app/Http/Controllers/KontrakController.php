c72e2da84a246c1c59e27cede85fed5f52a6d42b
Millinov <milinovadmiral@gmail.com>
2023-04-13 17:34:50 +0700

    Initial commit

---
+++ app/Http/Controllers/KontrakController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kontrak;
+use App\Http\Requests\StoreKontrakRequest;
+use App\Http\Requests\UpdateKontrakRequest;
+
+class KontrakController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreKontrakRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreKontrakRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kontrak  $kontrak
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kontrak $kontrak)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kontrak  $kontrak
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kontrak $kontrak)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateKontrakRequest  $request
+     * @param  \App\Models\Kontrak  $kontrak
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateKontrakRequest $request, Kontrak $kontrak)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kontrak  $kontrak
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kontrak $kontrak)
+    {
+        //
+    }
+}
